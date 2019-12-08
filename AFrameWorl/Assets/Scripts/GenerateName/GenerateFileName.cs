e2f3a6f86515faa100e10a47a1069450f0ccfd99
anyue <1183767577@qq.com>
2019-12-08 15:55:07 +0800

    打开资源所在的文件夹。

--- AFrameWorl/Assets/Scripts/GenerateName/GenerateFileName.cs
+++ AFrameWorl/Assets/Scripts/GenerateName/GenerateFileName.cs
@@ -7,7 +7,7 @@ using UnityEngine;
  * @Date: 2019-12-08 14:39:05 
  * @Description: 生成文件名到剪切板
  * @Last Modified by: l hy
- * @Last Modified time: 2019-12-08 15:22:28
+ * @Last Modified time: 2019-12-08 15:53:12
  */
 
 public class GenerateFileName {
@@ -16,6 +16,7 @@ public class GenerateFileName {
     private static void spawnFileTime () {
 
         string filePathName = "D:/UnityWork/AFrameWork/" + "AFramework_" + DateTime.Now.ToString ("yyyyMMdd") + ".unitypackage";
+        string floderPath = "D:/UnityWork/AFrameWork/";
 
         string assetPathName = "Assets/Scripts";
 
@@ -23,6 +24,8 @@ public class GenerateFileName {
         // GUIUtility.systemCopyBuffer = fileTime;
 
         AssetDatabase.ExportPackage (assetPathName, filePathName, ExportPackageOptions.Recurse);
-        Debug.Log ("generate success");
+
+        // open package floder
+        Application.OpenURL("file:///"+floderPath);
     }
 }
