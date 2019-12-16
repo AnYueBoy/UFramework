b3ea10cd674ee263d522fc4817f9415683362b28
anyue <1183767577@qq.com>
2019-12-16 23:05:39 +0800

    添加识别横屏竖屏逻辑；添加清空控制台信息的逻辑。

---
+++ AFrameWorl/Assets/Scripts/Util/Util.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Util {
+
+    [MenuItem ("AFramework/isLandscape")]
+    public static void isLandscape () {
+        Util.clearConsole ();
+        float aspect = (float) Screen.width / Screen.height;
+        if (aspect > 1) {
+            Debug.Log ("is landscape");
+        } else {
+            Debug.Log ("is not landscape");
+        }
+
+        Debug.Log ("aspece is : " + aspect);
+    }
+
+    public static void clearConsole () {
+        Type log = typeof (EditorWindow).Assembly.GetType ("UnityEditor.LogEntries");
+
+        var clearMethod = log.GetMethod ("Clear");
+        clearMethod.Invoke (null, null);
+    }
+}
