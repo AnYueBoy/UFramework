b22f2c122840abb6e5cbce425c717fbf80690ecc
anyue <1183767577@qq.com>
2019-12-21 14:40:42 +0800

    添加归并排序算法。

--- AFrameWorl/Assets/Scripts/Util/Util.cs
+++ AFrameWorl/Assets/Scripts/Util/Util.cs
@@ -14,43 +14,98 @@ using UnityEngine;
  * @Last Modified time: 2019-12-16 23:05:55 
  */
 
-public class Util {
+public class Util
+{
 
-    public static float getAspect () {
-        float aspect = (float) Screen.width / Screen.height;
+    public static float getAspect()
+    {
+        float aspect = (float)Screen.width / Screen.height;
         return aspect;
     }
 
-    public static void clearConsole () {
-        Type log = typeof (EditorWindow).Assembly.GetType ("UnityEditor.LogEntries");
+    public static void clearConsole()
+    {
+        Type log = typeof(EditorWindow).Assembly.GetType("UnityEditor.LogEntries");
 
-        var clearMethod = log.GetMethod ("Clear");
-        clearMethod.Invoke (null, null);
+        var clearMethod = log.GetMethod("Clear");
+        clearMethod.Invoke(null, null);
     }
 
-    public static bool isLandscape () {
-        float aspect = getAspect ();
-        if (aspect > 1) {
+    public static bool isLandscape()
+    {
+        float aspect = getAspect();
+        if (aspect > 1)
+        {
             return true;
         }
 
         return false;
     }
 
-    public static bool isPadResoluation () {
-        return targetValue (4 / 3.0f);
+    public static bool isPadResoluation()
+    {
+        return targetValue(4 / 3.0f);
     }
 
-    public static bool isPhone () {
-        return targetValue (16 / 9.0f);
+    public static bool isPhone()
+    {
+        return targetValue(16 / 9.0f);
     }
 
-    public static bool targetValue (float value) {
+    public static bool targetValue(float value)
+    {
         float offset = 0.05f;
-        float aspect = getAspect ();
-        if (aspect > value - offset && aspect < value + offset) {
+        float aspect = getAspect();
+        if (aspect > value - offset && aspect < value + offset)
+        {
             return true;
         }
         return false;
+    }
+
+    public static void mergeSort(ArrayList targetArray)
+    {
+        mergeSort(targetArray, 0, targetArray.Capacity - 1);
+    }
+
+    private void mSort(ArrayList array, int l, int r)
+    {
+        if (l == r)
+        {
+            return;
+        }
+
+        // right move calculate
+        int mid = l + (r - l) >> 1;
+        mSort(array, l, mid);
+        mSort(array, mid + 1, r);
+        merge(array, l, mid, r);
+    }
+
+    private void merge(ArrayList array, int l, int mid, int r)
+    {
+        ArrayList temp = new ArrayList<int>(r - l + 1);
+        int leftPointer = l;
+        int rightPointer = mid + 1;
+        int index = 0;
+        while (leftPointer <= mid && rightPointer <= r)
+        {
+            temp[index++] = array[leftPointer] <= array[rightPointer] ? array[leftPointer++] : array[rightPointer++];
+        }
+
+        while (leftPointer <= mid)
+        {
+            temp[index++] = array[leftPointer++];
+        }
+
+        while (rightPointer <= r)
+        {
+            temp[index++] = array[rightPointer++];
+        }
+
+        for (int i = 0; i < temp.Capacity; i++)
+        {
+            array[l + i] = temp[i];
+        }
     }
 }
