fba18c21d905bbc74eb8260da1bb358aa8ed0389
anyue <1183767577@qq.com>
2019-12-30 22:45:31 +0800

    添加地图config文件导出逻辑。

---
+++ AFrameWorl/Assets/Scripts/ConfigData/NodeInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[SerializeField]
+public class NodeInfo {
+
+    public string nodeName = null;
+
+    public double x = 0;
+
+    public double y = 0;
+
+    public double rotation = 0;
+
+    public double scale = 0;
+}
---
+++ AFrameWorl/Assets/Scripts/MapBuild/MapBuild.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using LitJson;
+using UnityEditor;
+using UnityEngine;
+
+public class MapBuild {
+
+    [MenuItem ("Export/exprot node to json")]
+    private static void exprotJson () {
+
+        Transform targetNode = GameObject.Find ("Obstacles").transform;
+
+        NodeList nodeList = new NodeList ();
+
+        foreach (Transform transform in targetNode) {
+            NodeInfo nodeInfo = new NodeInfo ();
+            nodeInfo.nodeName = transform.name;
+            nodeInfo.x = transform.position.x / 100;
+            nodeInfo.y = transform.position.y / 100;
+            nodeInfo.rotation = transform.rotation.z;
+            nodeInfo.scale = transform.localScale.x;
+
+            nodeList.nodeList.Add (nodeInfo);
+        }
+
+        string filePath = Application.dataPath + "/MapConfig" + "/MapConfig.json";
+
+        FileInfo fileInfo = new FileInfo (filePath);
+        if (!File.Exists (filePath)) {
+            File.Delete (filePath);
+        }
+
+        StreamWriter sw = new StreamWriter (filePath);
+
+        string jsonData = JsonMapper.ToJson (nodeList);
+
+        sw.Write (jsonData);
+        sw.Close ();
+
+        AssetDatabase.Refresh ();
+    }
+}
