c3496b42e78668353c6e76ea3ff81bfd5fd3bba9
anyue <1183767577@qq.com>
2020-01-16 17:00:37 +0800

    优化数据流读取逻辑。

--- AFrameWorl/Assets/Scripts/MapBuild/MapRecovery.cs
+++ AFrameWorl/Assets/Scripts/MapBuild/MapRecovery.cs
@@ -1,8 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using LitJson;
 using UnityEngine;
 
-public class MapRecovery : MonoBehaviour
-{
-   
+public class MapRecovery : MonoBehaviour {
+
+    public static string recovery (int level) {
+        string result = "还原成功";
+        string filePath = Application.dataPath + "/MapConfig" + "/MapConfig.json";
+        if (!File.Exists (filePath)) {
+            result = "配置文件不存在,检查路径,当前路径:" + filePath;
+            return result;
+        }
+
+        // 流数据读取
+        StreamReader sr = new StreamReader (filePath);
+        string context = sr.ReadToEnd ();
+        sr.Close ();
+
+        NodeList nodeList = JsonMapper.ToObject<NodeList> (context);
+
+        Transform obstacleTrans = GameObject.Find ("Obstacles").transform;
+        while (obstacleTrans.childCount > 0) {
+            foreach (Transform obstacle in obstacleTrans) {
+                GameObject.DestroyImmediate (obstacle.gameObject);
+            }
+        }
+
+        // for (int i = 0; i < nodeList)
+
+        return result;
+    }
 }
