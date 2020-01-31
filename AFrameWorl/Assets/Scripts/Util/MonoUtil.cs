a11cc795b56a38db50c9e3a778c6e2b39b035382
anyue <1183767577@qq.com>
2020-01-31 14:27:05 +0800

    添加定时器方法。

---
+++ AFrameWorl/Assets/Scripts/Util/MonoUtil.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonoUtil : MonoBehaviour {
+
+    public void deleay (float time, Action onFinished) {
+        StartCoroutine (deleayCoroutine (time, onFinished));
+    }
+
+    private IEnumerator deleayCoroutine (float time, Action finished) {
+        yield return new WaitForSeconds (time);
+        finished ();
+    }
+}
