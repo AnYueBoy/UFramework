096ef7d1794ac06bacd2516caa109e6ee7052acd
anyue <1183767577@qq.com>
2020-03-07 17:20:52 +0800

    添加界面管理。

---
+++ AFrameWorl/Assets/Scripts/GameCommon/BaseUI.cs
@@ -0,0 +1,36 @@
+﻿/*
+ * @Author: l hy 
+ * @Date: 2020-03-07 16:37:25 
+ * @Description: 界面基类 
+ * @Last Modified by: l hy
+ * @Last Modified time: 2020-03-07 17:11:31
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseUI : MonoBehaviour {
+
+    protected static string resUrl = "BaseUI";
+
+    private object myTag;
+
+    public object tag {
+        get {
+            return this.myTag;
+        }
+
+        set {
+            this.myTag = value;
+        }
+    }
+
+    public static string getUrl () {
+        return "" + resUrl;
+    }
+
+    public void onShow (params object[] args) {
+
+    }
+}
---
+++ AFrameWorl/Assets/Scripts/GameCommon/UIManager.cs
@@ -0,0 +1,37 @@
+﻿/*
+ * @Author: l hy 
+ * @Date: 2020-03-07 17:13:07 
+ * @Description: 界面管理器 
+ * @Last Modified by: l hy
+ * @Last Modified time: 2020-03-07 17:18:01
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour {
+
+    private static UIManager instance = null;
+
+    private List<BaseUI> uiList = new List<BaseUI> ();
+
+    private BaseUI currentBoard = null;
+
+    private BaseUI currentDialog = null;
+
+    [Header ("uiRoot")]
+    public GameObject uiRoot;
+
+    public static UIManager getInstance () {
+        return instance;
+    }
+
+    private void Awake () {
+        if (instance == null) {
+            instance = this;
+        }
+
+        this.currentBoard = null;
+    }
+}
