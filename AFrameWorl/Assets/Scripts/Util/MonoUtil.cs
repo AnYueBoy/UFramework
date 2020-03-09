004f2716b7e36e01c962ab2bfae162c5f6520758
anyue <1183767577@qq.com>
2020-03-09 23:42:53 +0800

    添加绘制函数。

--- AFrameWorl/Assets/Scripts/Util/MonoUtil.cs
+++ AFrameWorl/Assets/Scripts/Util/MonoUtil.cs
@@ -13,4 +13,35 @@ public class MonoUtil : MonoBehaviour {
         yield return new WaitForSeconds (time);
         finished ();
     }
+
+    private Vector2[] pathList = null;
+
+    /// <summary>
+    /// 绘制路径
+    /// </summary>
+    /// <param name="path">路径信息</param>
+    protected void drawLine (Vector2[] path) {
+        this.pathList = path;
+    }
+
+    private void OnDrawGizmos () {
+        if (this.pathList == null || this.pathList.Length <= 0) {
+            return;
+        }
+
+        Color orginColor = Gizmos.color;
+        Gizmos.color = Color.red;
+        for (int i = 0; i < this.pathList.Length; i++) {
+            Vector2 point = this.pathList[i];
+            Gizmos.DrawSphere (point, 0.2f);
+        }
+
+        Gizmos.color = Color.yellow;
+        for (int i = 0; i < this.pathList.Length - 1; i++) {
+            Gizmos.DrawLine (this.pathList[i], this.pathList[i + 1]);
+        }
+
+        Gizmos.color = orginColor;
+
+    }
 }
---
+++ AFrameWorl/Assets/Test.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoUtil
+{
+    // Start is called before the first frame update
+
+
+    void Start()
+    {
+        Vector2[] path =new Vector2[5];
+        path[1] = new Vector2(0,5);
+        path[2] = new Vector2(5,10);
+        path[3] = new Vector2(10,15);
+        this.drawLine(path);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
