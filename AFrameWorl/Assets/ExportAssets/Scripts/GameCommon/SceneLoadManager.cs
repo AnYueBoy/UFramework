a85829270084400fa21bce9e759327b8358cea96
anyue <1183767577@qq.com>
2020-04-10 22:51:00 +0800

    更新场景加载管理逻辑。

---
+++ AFrameWorl/Assets/ExportAssets/Scripts/GameCommon/SceneLoadManager.cs
@@ -0,0 +1,47 @@
+﻿/*
+ * @Author: l hy 
+ * @Date: 2020-04-10 21:54:42 
+ * @Description: 场景管理器 
+ * @Last Modified by: l hy
+ * @Last Modified time: 2020-04-10 22:34:12
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoadManager {
+
+    private static SceneLoadManager instance = null;
+
+    public static SceneLoadManager getInstance () {
+        if (instance == null) {
+            instance = new SceneLoadManager ();
+        }
+
+        return instance;
+    }
+
+    /// <summary>
+    /// 切换下一场景
+    /// </summary>
+    public void loadNextScene () {
+        int currentSceneIndex = SceneManager.GetActiveScene ().buildIndex;
+        int resultSceneIndex = currentSceneIndex + 1;
+        int allSceneCount = SceneManager.sceneCountInBuildSettings;
+        if (resultSceneIndex > allSceneCount) {
+            resultSceneIndex = allSceneCount;
+        }
+
+        SceneManager.LoadScene (resultSceneIndex);
+    }
+
+    public void loadAppointScene (string sceneName) {
+        if (sceneName == "" || sceneName == null) {
+            return;
+        }
+
+        SceneManager.LoadScene (sceneName);
+    }
+}
