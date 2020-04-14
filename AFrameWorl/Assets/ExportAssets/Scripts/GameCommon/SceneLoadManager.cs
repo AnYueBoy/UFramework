ed2a07c6dacaa7191db993393cb9ec9517d53126
anyue <1183767577@qq.com>
2020-04-14 21:51:16 +0800

    场景加载管理。

--- AFrameWorl/Assets/ExportAssets/Scripts/GameCommon/SceneLoadManager.cs
+++ AFrameWorl/Assets/ExportAssets/Scripts/GameCommon/SceneLoadManager.cs
@@ -1,9 +1,10 @@
-﻿/*
+﻿using System.Linq;
+/*
  * @Author: l hy 
  * @Date: 2020-04-10 21:54:42 
  * @Description: 场景管理器 
  * @Last Modified by: l hy
- * @Last Modified time: 2020-04-10 22:34:12
+ * @Last Modified time: 2020-04-14 21:50:19
  */
 
 using System.Collections;
@@ -26,19 +27,47 @@ public class SceneLoadManager {
     /// <summary>
     /// 切换下一场景
     /// </summary>
-    public void loadNextScene () {
-        int currentSceneIndex = SceneManager.GetActiveScene ().buildIndex;
-        int resultSceneIndex = currentSceneIndex + 1;
-        int allSceneCount = SceneManager.sceneCountInBuildSettings;
-        if (resultSceneIndex > allSceneCount) {
-            resultSceneIndex = allSceneCount;
+    public void loadNextScene (bool isSceneIndex = true) {
+        if (isSceneIndex) {
+            int currentSceneIndex = SceneManager.GetActiveScene ().buildIndex;
+            int resultSceneIndex = currentSceneIndex + 1;
+            int allSceneCount = SceneManager.sceneCountInBuildSettings;
+            if (resultSceneIndex > allSceneCount) {
+                resultSceneIndex = allSceneCount;
+            }
+
+            SceneManager.LoadScene (resultSceneIndex);
+            return;
         }
 
-        SceneManager.LoadScene (resultSceneIndex);
+        string nextSceneName = null;
+        string currentSceneName = SceneManager.GetActiveScene ().name;
+        for (int i = 0; i < SceneConfig.sceneConfigs.Count (); i++) {
+            string sceneName = SceneConfig.sceneConfigs[i];
+            if (string.IsNullOrEmpty (sceneName)) {
+                continue;
+            }
+
+            if (currentSceneName == sceneName) {
+                if (i >= SceneConfig.sceneConfigs.Count ()) {
+                    break;
+                }
+
+                nextSceneName = SceneConfig.sceneConfigs[i + 1];
+                break;
+            }
+        }
+
+        if (string.IsNullOrEmpty (nextSceneName)) {
+            Debug.LogError ("load nextSceneName is null");
+            return;
+        }
+
+        SceneManager.LoadScene (nextSceneName);
     }
 
     public void loadAppointScene (string sceneName) {
-        if (sceneName == "" || sceneName == null) {
+        if (string.IsNullOrEmpty (sceneName)) {
             return;
         }
 
