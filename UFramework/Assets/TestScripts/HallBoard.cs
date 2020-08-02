3d6d47d986ee190d9990b47bff7abc5d552cef71
DarkMoon <1183767577@qq.com>
2020-08-02 18:27:53 +0800

    添加音效测试代码。

--- UFramework/Assets/TestScripts/HallBoard.cs
+++ UFramework/Assets/TestScripts/HallBoard.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class HallBoard : MonoBehaviour {
     // Start is called before the first frame update
     void Start () {
-        AudioManager.getInstance ().playSoundByName ("test");
+        AudioManager.getInstance ().playSound ("test");
     }
 
     // Update is called once per frame
@@ -16,5 +16,21 @@ public class HallBoard : MonoBehaviour {
 
     public void loadNextScene () {
         SceneLoadManager.getInstance ().loadNextScene (false, (AsyncOperation asy) => { Debug.Log ("加载完成"); });
+    }
+
+    public void playBGM () {
+        AudioManager.getInstance ().playMusic ("BGM");
+    }
+
+    public void pauseBGM () {
+        AudioManager.getInstance ().pauseMusic ();
+    }
+
+    public void stopBGM () {
+        AudioManager.getInstance ().stopMusic ();
+    }
+
+     public void resumeBGM () {
+        AudioManager.getInstance ().resumeMusic ();
     }
 }
