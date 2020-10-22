b450a11e8fdc9d4c233df2e858bafb88e6467146
DarkMoon <1183767577@qq.com>
2020-10-22 22:28:38 +0800

    添加资源管理模块。

--- UFramework/Assets/UFramework/Scripts/GameCommon/AssetsManager.cs
+++ UFramework/Assets/UFramework/Scripts/GameCommon/AssetsManager.cs
@@ -6,7 +6,6 @@
  * @Last Modified time: 2020-10-10 07:27:03
  */
 
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -18,7 +17,6 @@ public class AssetsManager {
 
     public T getAssetsByUrl<T> (string assetsUrl) where T : Object {
         Object targetAssets = null;
-        this.addRef (assetsUrl);
         if (assetsPool.ContainsKey (assetsUrl)) {
             targetAssets = assetsPool[assetsUrl];
             return targetAssets as T;
@@ -26,16 +24,26 @@ public class AssetsManager {
 
         targetAssets = AssetsLoadManager.loadAssets<T> (assetsUrl);
         assetsPool.Add (assetsUrl, targetAssets);
-
+        this.addRef (assetsUrl);
         return targetAssets as T;
     }
 
-    // FIXME: 等待完善
-    // public int unLoadAssetByUrl (string assetsUrl) {
-    //     if (!referenceCounter.ContainsKey (assetsUrl)) {
-    //         Resources.UnloadUnusedAssets ();
-    //     }
-    // }
+    public void tryReleaseAssets (string assetsUrl) {
+        int result = this.redRef (assetsUrl);
+        if (result <= -1) {
+            Debug.LogError ("release assets error assets not exist");
+            return;
+        }
+
+        if (result == 0) {
+            Object assets = assetsPool[assetsUrl];
+            assetsPool.Remove (assetsUrl);
+            Resources.UnloadAsset (assets);
+            return;
+        }
+
+        Debug.LogWarning ("release assets error exist not zero ref");
+    }
 
     private void addRef (string assetsUrl) {
         if (referenceCounter.ContainsKey (assetsUrl)) {
@@ -43,5 +51,19 @@ public class AssetsManager {
         } else {
             referenceCounter.Add (assetsUrl, 1);
         }
+    }
+
+    private int redRef (string assetsUrl) {
+        if (!referenceCounter.ContainsKey (assetsUrl)) {
+            return -1;
+        }
+
+        referenceCounter[assetsUrl]--;
+        if (referenceCounter[assetsUrl] <= 0) {
+            referenceCounter.Remove (assetsUrl);
+            return 0;
+        }
+
+        return referenceCounter[assetsUrl];
     }
 }
