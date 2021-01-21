83ae8592bb1e5877d806bf4e855775da3a9a0f12
anyue <1183767577@qq.com>
2021-01-21 21:49:14 +0800

    添加调试信息管理逻辑

---
+++ UFramework/Assets/UFramework/Scripts/GameCommon/DebugLogManager.cs
@@ -0,0 +1,14 @@
+﻿/*
+ * @Author: l hy 
+ * @Date: 2021-01-21 21:47:35 
+ * @Description: 调试信息管理
+ * @Last Modified by: l hy
+ * @Last Modified time: 2021-01-21 21:48:37
+ */
+
+namespace UFrameWork.GameCommon {
+    using UnityEngine;
+    public class DebugLogManager : MonoBehaviour {
+
+    }
+}
