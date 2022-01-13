300b4f97a5f52606945c81474bc1eb49fef114fa
anyue <1183767577@qq.com>
2022-01-13 15:44:47 +0800

    删除zenject

--- UFramework/Assets/TestScripts/JectTwoManager.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using Zenject;
-public class JectTwoManager {
-    public void connect () {
-        Debug.Log ("ject two connected success");
-    }
-}
