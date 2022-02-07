9596e30455102fb66d54d6fc5f3e87e903fcb698
anyue <1183767577@qq.com>
2022-02-07 19:46:53 +0800

    临时提交

---
+++ UFramework/Assets/UFramework/Scripts/Util/Singleton.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Reflection;
+namespace UFramework.Util {
+
+    public class Singleton<T> where T : Singleton<T> {
+        private static T _instance;
+
+        public static T Instance {
+            get {
+                if (_instance == null) {
+                    Type type = typeof (T);
+                    ConstructorInfo[] constructors = type.GetConstructors (BindingFlags.Instance | BindingFlags.NonPublic);
+                    ConstructorInfo constructor = Array.Find (constructors, (c) => c.GetParameters ().Length == 0);
+                    if (constructor == null) {
+                        throw new System.Exception ($"{type} cannot find non param constructor.");
+                    }
+
+                    _instance = constructor.Invoke (null) as T;
+                }
+                return _instance;
+            }
+        }
+
+    }
+
+}
