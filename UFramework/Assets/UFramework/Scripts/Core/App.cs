9d26b1d423775dd4d876d1d8bef2b1e003735e63
anyue <1183767577@qq.com>
2022-02-16 09:11:44 +0800

    删除注释

--- UFramework/Assets/UFramework/Scripts/Core/App.cs
+++ UFramework/Assets/UFramework/Scripts/Core/App.cs
@@ -2,17 +2,14 @@ using System;
 using UFramework.Container;
 
 namespace UFramework.Core {
-    /// <summary>
-    /// The <see cref="IApplication"/> static facade.
-    /// </summary>
     public abstract class App {
         /// <summary>
-        /// The <see cref="IApplication"/> instance.
+        /// The IApplication instance.
         /// </summary>
         private static IApplication that;
 
         /// <summary>
-        /// Callback when a new <see cref="IApplication"/> instance is created.
+        /// Callback when a new IApplication instance is created.
         /// </summary>
         public static event Action<IApplication> OnNewApplication {
             add {
@@ -25,12 +22,12 @@ namespace UFramework.Core {
         }
 
         /// <summary>
-        /// Callback when a new <see cref="IApplication"/> instance is created.
+        /// Callback when a new IApplication instance is created.
         /// </summary>
         private static event Action<IApplication> RaiseOnNewApplication;
 
         /// <summary>
-        /// Gets or Sets the <see cref="IApplication"/> instance.
+        /// Gets or Sets the IApplication instance.
         /// </summary>
         public static IApplication That {
             get {
@@ -43,61 +40,49 @@ namespace UFramework.Core {
             }
         }
 
-        /// <inheritdoc cref="IApplication.IsMainThread"/>
         public static bool IsMainThread => That.IsMainThread;
 
-        /// <inheritdoc cref="IApplication.DebugLevel"/>
         public static DebugLevel DebugLevel {
             get => That.DebugLevel;
             set => That.DebugLevel = value;
         }
 
-        /// <inheritdoc cref="IApplication.Terminate"/>
         public static void Terminate () {
             That.Terminate ();
         }
 
-        /// <inheritdoc cref="IApplication.Register"/>
         public static void Register (IServiceProvider provider, bool force = false) {
             That.Register (provider, force);
         }
 
-        /// <inheritdoc cref="IApplication.IsRegistered"/>
         public static bool IsRegistered (IServiceProvider provider) {
             return That.IsRegistered (provider);
         }
 
-        /// <inheritdoc cref="IApplication.GetRuntimeId"/>
         public static long GetRuntimeId () {
             return That.GetRuntimeId ();
         }
 
-        /// <inheritdoc cref="IContainer.OnFindType(Func{string, Type}, int)"/>
         public static IContainer OnFindType (Func<string, Type> func, int priority = int.MaxValue) {
             return That.OnFindType (func, priority);
         }
 
-        /// <inheritdoc cref="IContainer.OnRebound(string, Action{object})"/>
         public static IContainer OnRebound (string service, Action<object> callback) {
             return That.OnRebound (service, callback);
         }
 
-        /// <inheritdoc cref="IContainer.GetBind"/>
         public static IBindData GetBind (string service) {
             return That.GetBind (service);
         }
 
-        /// <inheritdoc cref="ExtendContainer.GetBind{TService}(IContainer)"/>
         public static IBindData GetBind<TService> () {
             return That.GetBind<TService> ();
         }
 
-        /// <inheritdoc cref="IContainer.HasInstance(string)"/>
         public static bool HasInstance (string service) {
             return That.HasInstance (service);
         }
 
-        /// <inheritdoc cref="ExtendContainer.HasInstance{TService}(IContainer)"/>
         public static bool HasInstance<TService> () {
 #if CATLIB_PERFORMANCE
             return Facade<TService>.HasInstance || Handler.HasInstance<TService> ();
@@ -106,192 +91,154 @@ namespace UFramework.Core {
 #endif
         }
 
-        /// <inheritdoc cref="IContainer.IsResolved(string)"/>
         public static bool IsResolved (string service) {
             return That.IsResolved (service);
         }
 
-        /// <inheritdoc cref="ExtendContainer.IsResolved{TService}(IContainer)"/>
         public static bool IsResolved<TService> () {
             return That.IsResolved<TService> ();
         }
 
-        /// <inheritdoc cref="IContainer.HasBind(string)"/>
         public static bool HasBind (string service) {
             return That.HasBind (service);
         }
 
-        /// <inheritdoc cref="ExtendContainer.HasBind{TService}(IContainer)"/>
         public static bool HasBind<TService> () {
             return That.HasBind<TService> ();
         }
 
-        /// <inheritdoc cref="IContainer.CanMake(string)"/>
         public static bool CanMake (string service) {
             return That.CanMake (service);
         }
 
-        /// <inheritdoc cref="ExtendContainer.CanMake{TService}(IContainer)"/>
         public static bool CanMake<TService> () {
             return That.CanMake<TService> ();
         }
 
-        /// <inheritdoc cref="IContainer.IsStatic(string)"/>
         public static bool IsStatic (string service) {
             return That.IsStatic (service);
         }
 
-        /// <inheritdoc cref="ExtendContainer.IsStatic{TService}(IContainer)"/>
         public static bool IsStatic<TService> () {
             return That.IsStatic<TService> ();
         }
 
-        /// <inheritdoc cref="IContainer.IsAlias(string)"/>
         public static bool IsAlias (string name) {
             return That.IsAlias (name);
         }
 
-        /// <inheritdoc cref="ExtendContainer.IsAlias{TService}(IContainer)"/>
         public static bool IsAlias<TService> () {
             return That.IsAlias<TService> ();
         }
 
-        /// <inheritdoc cref="IContainer.Alias(string, string)"/>
         public static IContainer Alias (string alias, string service) {
             return That.Alias (alias, service);
         }
 
-        /// <inheritdoc cref="ExtendContainer.Alias{TAlias, TService}(IContainer)"/>
         public static IContainer Alias<TAlias, TService> () {
             return That.Alias<TAlias, TService> ();
         }
 
-        /// <inheritdoc cref="ExtendContainer.Bind{TService}(IContainer)"/>
         public static IBindData Bind<TService> () {
             return That.Bind<TService> ();
         }
 
-        /// <inheritdoc cref="ExtendContainer.Bind{TService, TConcrete}(IContainer)"/>
         public static IBindData Bind<TService, TConcrete> () {
             return That.Bind<TService, TConcrete> ();
         }
 
-        /// <inheritdoc cref="IContainer.Bind(string, Type, bool)"/>
         public static IBindData Bind (string service, Type concrete, bool isStatic) {
             return That.Bind (service, concrete, isStatic);
         }
 
-        /// <inheritdoc cref="IContainer.Bind(string, Func{IContainer, object[], object}, bool)"/>
         public static IBindData Bind (string service, Func<IContainer, object[], object> concrete, bool isStatic) {
             return That.Bind (service, concrete, isStatic);
         }
 
-        /// <inheritdoc cref="ExtendContainer.Bind(IContainer, Func{IContainer, object[], object})"/>
         public static IBindData Bind<TService> (Func<IContainer, object[], object> concrete) {
             return That.Bind<TService> (concrete);
         }
 
-        /// <inheritdoc cref="ExtendContainer.Bind{TService}(IContainer, Func{object[], object})"/>
         public static IBindData Bind<TService> (Func<object[], object> concrete) {
             return That.Bind<TService> (concrete);
         }
 
-        /// <inheritdoc cref="ExtendContainer.Bind{TService}(IContainer, Func{object})"/>
         public static IBindData Bind<TService> (Func<object> concrete) {
             return That.Bind<TService> (concrete);
         }
 
-        /// <inheritdoc cref="ExtendContainer.Bind(IContainer, string, Func{IContainer, object[], object})"/>
         public static IBindData Bind (string service, Func<IContainer, object[], object> concrete) {
             return That.Bind (service, concrete);
         }
 
-        /// <inheritdoc cref="ExtendContainer.Singleton{TService, TConcrete}(IContainer)"/>
         public static IBindData Singleton<TService, TConcrete> () {
             return That.Singleton<TService, TConcrete> ();
         }
 
-        /// <inheritdoc cref="ExtendContainer.Singleton{TService}(IContainer)"/>
         public static IBindData Singleton<TService> () {
             return That.Singleton<TService> ();
         }
 
-        /// <inheritdoc cref="ExtendContainer.Singleton{TService}(IContainer, Func{IContainer, object[], object})"/>
         public static IBindData Singleton<TService> (Func<IContainer, object[], object> concrete) {
             return That.Singleton<TService> (concrete);
         }
 
-        /// <inheritdoc cref="ExtendContainer.Singleton{TService}(IContainer, Func{object[], object})"/>
         public static IBindData Singleton<TService> (Func<object[], object> concrete) {
             return That.Singleton<TService> (concrete);
         }
 
-        /// <inheritdoc cref="ExtendContainer.Singleton{TService}(IContainer, Func{object})"/>
         public static IBindData Singleton<TService> (Func<object> concrete) {
             return That.Singleton<TService> (concrete);
         }
 
-        /// <inheritdoc cref="ExtendContainer.Singleton(IContainer, string, Func{IContainer, object[], object})"/>
         public static IBindData Singleton (string service, Func<IContainer, object[], object> concrete) {
             return That.Singleton (service, concrete);
         }
 
-        /// <inheritdoc cref="IContainer.Unbind(string)"/>
         public static void Unbind (string service) {
             That.Unbind (service);
         }
 
-        /// <inheritdoc cref="ExtendContainer.Unbind{TService}(IContainer)"/>
         public static void Unbind<TService> () {
             That.Unbind<TService> ();
         }
 
-        /// <inheritdoc cref="IContainer.Tagged(string)"/>
         public static object[] Tagged (string tag) {
             return That.Tagged (tag);
         }
 
-        /// <inheritdoc cref="IContainer.Tag(string, string[])"/>
         public static void Tag (string tag, params string[] service) {
             That.Tag (tag, service);
         }
 
-        /// <inheritdoc cref="ExtendContainer.Tag{TService}(IContainer, string)"/>
         public static void Tag<TService> (string tag) {
             That.Tag<TService> (tag);
         }
 
-        /// <inheritdoc cref="IContainer.Instance(string, object)"/>
         public static object Instance (string service, object instance) {
             return That.Instance (service, instance);
         }
 
-        /// <inheritdoc cref="ExtendContainer.Instance{TService}(IContainer, object)"/>
         public static void Instance<TService> (object instance) {
             That.Instance<TService> (instance);
         }
 
-        /// <inheritdoc cref="IContainer.Release(object)"/>
         public static bool Release (string service) {
             return That.Release (service);
         }
 
-        /// <inheritdoc cref="ExtendContainer.Release{TService}(IContainer)"/>
         public static bool Release<TService> () {
             return That.Release<TService> ();
         }
 
-        /// <inheritdoc cref="ExtendContainer.Release(IContainer, ref object[], bool)"/>
         public static bool Release (ref object[] instances, bool reverse = true) {
             return That.Release (ref instances, reverse);
         }
 
-        /// <inheritdoc cref="IContainer.Make(string, object[])"/>
         public static object Make (string service, params object[] userParams) {
             return That.Make (service, userParams);
         }
 
-        /// <inheritdoc cref="ExtendContainer.Make{TService}(IContainer, object[])"/>
         public static TService Make<TService> (params object[] userParams) {
 #if CATLIB_PERFORMANCE
             return Facade<TService>.Make (userParams);
@@ -300,97 +247,78 @@ namespace UFramework.Core {
 #endif
         }
 
-        /// <inheritdoc cref="ExtendContainer.Make(IContainer, Type, object[])"/>
         public static object Make (Type type, params object[] userParams) {
             return That.Make (type, userParams);
         }
 
-        /// <inheritdoc cref="ExtendContainer.Factory(IContainer, string, object[])"/>
         public static Func<object> Factory (string service, params object[] userParams) {
             return That.Factory (service, userParams);
         }
 
-        /// <inheritdoc cref="ExtendContainer.Factory{TService}(IContainer, object[])"/>
         public static Func<TService> Factory<TService> (params object[] userParams) {
             return That.Factory<TService> (userParams);
         }
 
-        /// <inheritdoc cref="IContainer.OnRelease(Action{IBindData, object})"/>
         public static IContainer OnRelease (Action<IBindData, object> action) {
             return That.OnRelease (action);
         }
 
-        /// <inheritdoc cref="ExtendContainer.OnRelease(IContainer, Action{object})"/>
         public static IContainer OnRelease (Action<object> callback) {
             return That.OnRelease (callback);
         }
 
-        /// <inheritdoc cref="ExtendContainer.OnRelease{T}(IContainer, Action{T})"/>
         public static IContainer OnRelease<TWhere> (Action<TWhere> closure) {
             return That.OnRelease (closure);
         }
 
-        /// <inheritdoc cref="ExtendContainer.OnRelease{T}(IContainer, Action{IBindData, T})"/>
         public static IContainer OnRelease<TWhere> (Action<IBindData, TWhere> closure) {
             return That.OnRelease (closure);
         }
 
-        /// <inheritdoc cref="IContainer.OnResolving(Action{IBindData, object})"/>
         public static IContainer OnResolving (Action<IBindData, object> closure) {
             return That.OnResolving (closure);
         }
 
-        /// <inheritdoc cref="ExtendContainer.OnRelease(IContainer, Action{object})"/>
         public static IContainer OnResolving (Action<object> callback) {
             return That.OnResolving (callback);
         }
 
-        /// <inheritdoc cref="ExtendContainer.OnResolving{T}(IContainer, Action{T})"/>
         public static IContainer OnResolving<TWhere> (Action<TWhere> closure) {
             return That.OnResolving (closure);
         }
 
-        /// <inheritdoc cref="ExtendContainer.OnResolving{T}(IContainer, Action{IBindData, T})"/>
         public static IContainer OnResolving<TWhere> (Action<IBindData, TWhere> closure) {
             return That.OnResolving (closure);
         }
 
-        /// <inheritdoc cref="IContainer.OnAfterResolving(Action{IBindData, object})"/>
         public static IContainer OnAfterResolving (Action<IBindData, object> closure) {
             return That.OnAfterResolving (closure);
         }
 
-        /// <inheritdoc cref="ExtendContainer.OnAfterResolving(IContainer, Action{object})"/>
         public static IContainer OnAfterResolving (Action<object> closure) {
             return That.OnAfterResolving (closure);
         }
 
-        /// <inheritdoc cref="ExtendContainer.OnAfterResolving{T}(IContainer, Action{T})"/>
         public static IContainer OnAfterResolving<TWhere> (Action<TWhere> closure) {
             return That.OnAfterResolving (closure);
         }
 
-        /// <inheritdoc cref="ExtendContainer.OnAfterResolving{T}(IContainer, Action{IBindData, T})"/>
         public static IContainer OnAfterResolving<TWhere> (Action<IBindData, TWhere> closure) {
             return That.OnAfterResolving (closure);
         }
 
-        /// <inheritdoc cref="ExtendContainer.Watch{TService}(IContainer, Action)"/>
         public static void Watch<TService> (Action method) {
             That.Watch<TService> (method);
         }
 
-        /// <inheritdoc cref="ExtendContainer.Watch{TService}(IContainer, Action{TService})"/>
         public static void Watch<TService> (Action<TService> method) {
             That.Watch (method);
         }
 
-        /// <inheritdoc cref="IContainer.Type2Service(Type)"/>
         public static string Type2Service (Type type) {
             return That.Type2Service (type);
         }
 
-        /// <inheritdoc cref="ExtendContainer.Type2Service{TService}(IContainer)"/>
         public static string Type2Service<TService> () {
             return That.Type2Service<TService> ();
         }
--- UFramework/Assets/UFramework/Scripts/Core/IBootstrap.cs
+++ UFramework/Assets/UFramework/Scripts/Core/IBootstrap.cs
@@ -1,7 +1,4 @@
 namespace UFramework.Core {
-    /// <summary>
-    /// <see cref="IBootstrap"/> is the interface implemented by all bootstrap classes.
-    /// </summary>
     public interface IBootstrap {
         /// <summary>
         /// Bootstrap the framework.
