a828096a8a29c26fb47e0044e436f7f6e04ce622
anyue <1183767577@qq.com>
2022-03-28 13:54:19 +0800

    删除Release扩展

--- UFramework/Assets/UFramework/Scripts/Container/ContainerExtension.cs
+++ UFramework/Assets/UFramework/Scripts/Container/ContainerExtension.cs
@@ -52,65 +52,10 @@ namespace UFramework.Container {
             return container.Instance (container.Type2Service (typeof (TService)), instance);
         }
 
-        public static bool Release<TService> (this IContainer container) {
-            return container.Release (container.Type2Service (typeof (TService)));
-        }
-
-        /// <summary>
-        /// Release an existing instance in the container.
-        /// </summary>
-        /// <param name="container">The <see cref="IContainer"/> instance.</param>
-        /// <param name="instances">Service instance that needs to be released.</param>
-        /// <param name="reverse">Whether to reverse the release order.</param>
-        /// <returns>Returns false if one has not been successfully released, <paramref name="instances"/> is an instance that has not been released.</returns>
-        public static bool Release (this IContainer container, ref object[] instances, bool reverse = true) {
-            if (instances == null || instances.Length <= 0) {
-                return true;
-            }
-
-            if (reverse) {
-                Array.Reverse (instances);
-            }
-
-            var errorIndex = 0;
-
-            for (var index = 0; index < instances.Length; index++) {
-                if (instances[index] == null) {
-                    continue;
-                }
-
-                if (!container.Release (instances[index])) {
-                    instances[errorIndex++] = instances[index];
-                }
-            }
-
-            Array.Resize (ref instances, errorIndex);
-
-            if (reverse && errorIndex > 0) {
-                Array.Reverse (instances);
-            }
-
-            return errorIndex <= 0;
-        }
-
-        /// <summary>
-        /// Resolve the given service or alias from the container.
-        /// </summary>
-        /// <typeparam name="TService">The service name.</typeparam>
-        /// <param name="container">The <see cref="IContainer"/> instance.</param>
-        /// <param name="userParams">The user parameters.</param>
-        /// <returns>The serivce instance. Throw exception if the service can not resolved.</returns>
         public static TService Make<TService> (this IContainer container, params object[] userParams) {
             return (TService) container.Make (container.Type2Service (typeof (TService)), userParams);
         }
 
-        /// <summary>
-        /// Resolve the given service or alias from the container.
-        /// </summary>
-        /// <param name="container">The <see cref="IContainer"/> instance.</param>
-        /// <param name="type">The service type.</param>
-        /// <param name="userParams">The user parameters.</param>
-        /// <returns>The serivce instance. Throw exception if the service can not resolved.</returns>
         public static object Make (this IContainer container, Type type, params object[] userParams) {
             var service = container.Type2Service (type);
             container.Bind (service, type, false);
