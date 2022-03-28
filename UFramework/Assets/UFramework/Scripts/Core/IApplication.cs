ef4522fc004693cf1a32952c19e7d8c492c294d4
anyue <1183767577@qq.com>
2022-03-28 14:07:08 +0800

    删除部分注释

--- UFramework/Assets/UFramework/Scripts/Core/IApplication.cs
+++ UFramework/Assets/UFramework/Scripts/Core/IApplication.cs
@@ -1,43 +1,22 @@
 using UFramework.Container;
 using UFramework.EventDispatcher;
 
-namespace UFramework.Core {
-    public interface IApplication : IContainer {
-
-        /// <summary>
-        /// Gets a value indicating whether true if we're on the main thread.
-        /// </summary>
+namespace UFramework.Core
+{
+    public interface IApplication : IContainer
+    {
         bool IsMainThread { get; }
 
-        /// <summary>
-        /// Gets or sets the debug level.
-        /// </summary>
         DebugLevel DebugLevel { get; set; }
 
-        /// <summary>
-        /// Gets the event dispatcher.
-        /// </summary>
-        /// <returns>Returns event dispathcher instance, null if the dispatcher not found.</returns>
-        IEventDispatcher GetDispatcher ();
+        IEventDispatcher GetDispatcher();
 
-        /// <summary>
-        /// Register a service provider with the application.
-        /// </summary>
-        void Register (IServiceProvider provider, bool force = false);
+        void Register(IServiceProvider provider, bool force = false);
 
-        /// <summary>
-        /// Checks whether the given service provider is registered.
-        /// </summary>
-        bool IsRegistered (IServiceProvider provider);
+        bool IsRegistered(IServiceProvider provider);
 
-        /// <summary>
-        /// Gets the unique runtime id.
-        /// </summary>
-        long GetRuntimeId ();
+        long GetRuntimeId();
 
-        /// <summary>
-        /// Terminates the IApplication
-        /// </summary>
-        void Terminate ();
+        void Terminate();
     }
 }
