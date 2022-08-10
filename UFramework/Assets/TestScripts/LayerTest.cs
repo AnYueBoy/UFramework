37652785fce3eb2bb6b45e125109909b6688f04e
anyue <1183767577@qq.com>
2022-08-10 12:02:50 +0800

    调整插件；删除旧的事件派发逻辑

---
+++ UFramework/Assets/TestScripts/LayerTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LayerTest : MonoBehaviour
+{
+    [SerializeField] private int count;
+
+    private List<string> layerName = new List<string>()
+    {
+        "UI",
+        "Test"
+    };
+
+
+    private void Start()
+    {
+        layerTest();
+    }
+
+    private void layerTest()
+    {
+        for (int i = 0; i < count; i++)
+        {
+            gameObject.layer = LayerMask.NameToLayer(layerName[i % layerName.Count]);
+        }
+    }
+}
