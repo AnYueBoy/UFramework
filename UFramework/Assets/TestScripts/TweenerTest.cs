3671f758aad5da6cdf389193ff9fedebd44f0980
anyue <1183767577@qq.com>
2023-04-26 11:00:21 +0800

    添加测试逻辑

--- UFramework/Assets/TestScripts/TweenerTest.cs
+++ UFramework/Assets/TestScripts/TweenerTest.cs
@@ -27,6 +27,32 @@ public class TweenerTest : MonoBehaviour
         var s = new TweenerSequence();
         s.Append(imageTrans.TweenerLocalMove(Vector3.right * 100, 2.0f).SetInitialValue(Vector3.zero))
             .AppendInterval(1.5f)
-            .Append(imageTrans.TweenerLocalMoveY(100, 2.0f)).AppendCallback(() => { Debug.Log("队列结束"); });
+            .Append(imageTrans.TweenerLocalMoveY(100, 2.0f))
+            .Append(imageTrans.TweenerMove(Vector3.zero, 2.0f))
+            .Append(imageTrans.TweenerMove(Vector3.zero, 2.0f))
+            .AppendCallback(() => { Debug.Log("队列结束"); });
+    }
+
+    private void TweeneSequenceTest1()
+    {
+        var s = new TweenerSequence();
+        s.Append(imageTrans.TweenerLocalMove(Vector3.right * 100, 2.0f))
+            .Append(imageTrans.TweenerLocalMoveY(100, 2.0f))
+            .Append(imageTrans.TweenerMove(Vector3.zero, 2.0f))
+            .Append(imageTrans.TweenerMove(Vector3.zero, 2.0f));
+    }
+
+    private void TweeneCompleted()
+    {
+        imageTrans.TweenerLocalMove(Vector3.right * 100, 2.0f).OnCompleted(() =>
+        {
+            imageTrans.TweenerLocalMoveY(100, 2.0f).OnCompleted(() =>
+            {
+                imageTrans.TweenerMove(Vector3.zero, 2.0f).OnCompleted(() =>
+                {
+                    imageTrans.TweenerMove(Vector3.zero, 2.0f);
+                });
+            });
+        });
     }
 }
