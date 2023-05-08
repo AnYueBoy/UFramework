4e0cdc9c06ef61df851ed243389b64a16cf3f89a
anyue <1183767577@qq.com>
2023-05-08 10:05:01 +0800

    添加bindData扩展

--- UFramework/Assets/UFramework/Scripts/Core/Container/Extension/BindDataExtension.cs
+++ UFramework/Assets/UFramework/Scripts/Core/Container/Extension/BindDataExtension.cs
@@ -18,5 +18,107 @@ namespace UFramework.Core.Container
             Guard.Requires<ArgumentNullException>(closure != null);
             return bindData.OnResolving((_, instance) => { closure(); });
         }
+
+        public static IBindData OnResolving(this IBindData bindData, Action<object> closure)
+        {
+            Guard.Requires<ArgumentNullException>(closure != null);
+            return bindData.OnResolving((_, instance) => { closure(instance); });
+        }
+
+        public static IBindData OnResolving<T>(this IBindData bindData, Action<T> closure)
+        {
+            Guard.Requires<ArgumentNullException>(closure != null);
+            return bindData.OnResolving((_, instance) =>
+            {
+                if (instance is T)
+                {
+                    closure((T)instance);
+                }
+            });
+        }
+
+        public static IBindData OnResolving<T>(this IBindData bindData, Action<IBindData, T> closure)
+        {
+            Guard.Requires<ArgumentNullException>(closure != null);
+            return bindData.OnResolving((bind, instance) =>
+            {
+                if (instance is T)
+                {
+                    closure(bind, (T)instance);
+                }
+            });
+        }
+
+        public static IBindData OnAfterResolving(this IBindData bindData, Action closure)
+        {
+            Guard.Requires<ArgumentNullException>(closure != null);
+            return bindData.OnAfterResolving((_, instance) => { closure(); });
+        }
+
+        public static IBindData OnAfterResolving(this IBindData bindData, Action<object> closure)
+        {
+            Guard.Requires<ArgumentNullException>(closure != null);
+            return bindData.OnAfterResolving((_, instance) => { closure(instance); });
+        }
+
+        public static IBindData OnAfterResolving<T>(this IBindData bindData, Action<T> closure)
+        {
+            Guard.Requires<ArgumentNullException>(closure != null);
+            return bindData.OnAfterResolving((_, instance) =>
+            {
+                if (instance is T)
+                {
+                    closure((T)instance);
+                }
+            });
+        }
+
+        public static IBindData OnAfterResolving<T>(this IBindData bindData, Action<IBindData, T> closure)
+        {
+            Guard.Requires<ArgumentNullException>(closure != null);
+            return bindData.OnAfterResolving((bind, instance) =>
+            {
+                if (instance is T)
+                {
+                    closure(bind, (T)instance);
+                }
+            });
+        }
+
+        public static IBindData OnRelease(this IBindData bindData, Action closure)
+        {
+            Guard.Requires<ArgumentNullException>(closure != null);
+            return bindData.OnRelease((_, __) => { closure(); });
+        }
+
+        public static IBindData OnRelease(this IBindData bindData, Action<object> closure)
+        {
+            Guard.Requires<ArgumentNullException>(closure != null);
+            return bindData.OnRelease((_, instance) => { closure(instance); });
+        }
+
+        public static IBindData OnRelease<T>(this IBindData bindData, Action<T> closure)
+        {
+            Guard.Requires<ArgumentNullException>(closure != null);
+            return bindData.OnRelease((_, instance) =>
+            {
+                if (instance is T)
+                {
+                    closure((T)instance);
+                }
+            });
+        }
+
+        public static IBindData OnRelease<T>(this IBindData bindData, Action<IBindData, T> closure)
+        {
+            Guard.Requires<ArgumentNullException>(closure != null);
+            return bindData.OnRelease((bind, instance) =>
+            {
+                if (instance is T)
+                {
+                    closure(bind, (T)instance);
+                }
+            });
+        }
     }
 }
