8dda4fd4cc4a6c1b56f1bcba07fc144a1f6ac639
lhy <1183767577@qq.com>
2024-01-12 18:28:53 +0800

    临时提交

--- UFramework/Assets/UFramework/Scripts/Core/Container/Interface/IBindable.cs
+++ UFramework/Assets/UFramework/Scripts/Core/Container/Interface/IBindable.cs
@@ -11,17 +11,20 @@
         string Service { get; }
 
         /// <summary>
-        /// 服务容器
+        /// 获取服务所属的容器
         /// </summary>
         IContainer Container { get; }
 
         /// <summary>
-        /// 从容器中解绑服务，如果服务是单例则会被自动释放
+        /// 从容器中解绑服务，
+        /// 如果被绑定的类是单例化的且已经被构建，
+        /// 那么解除绑定时会自动释放已经被生成的单例，同时触发服务构建事件
         /// </summary>
         void Unbind();
     }
 
-    public interface IBindable<TReturn> : IBindable where TReturn : IBindable
+    public interface IBindable<TReturn> : IBindable
+        where TReturn : IBindable
     {
         /// <summary>
         /// 获取服务指定的需求上下文数据
--- UFramework/Assets/UFramework/Scripts/Core/IApplication.cs
+++ UFramework/Assets/UFramework/Scripts/Core/IApplication.cs
@@ -3,18 +3,36 @@ using UFramework.EventDispatcher;
 
 namespace UFramework.Core
 {
-    public interface IApplication : IContainer 
+    public interface IApplication : IContainer
     {
+        /// <summary>
+        /// 是否运行在主线程
+        /// </summary>
         bool IsMainThread { get; }
 
+        /// <summary>
+        /// 获取debug级别
+        /// </summary>
         DebugLevel DebugLevel { get; set; }
 
+        /// <summary>
+        /// 获取事件派发器
+        /// </summary>
         IEventDispatcher GetDispatcher();
 
+        /// <summary>
+        /// 通过Application注册服务提供者
+        /// </summary>
         void Register(IServiceProvider provider, bool force = false);
 
+        /// <summary>
+        /// 检查指定的服务提供者是否已注册
+        /// </summary>
         bool IsRegistered(IServiceProvider provider);
 
+        /// <summary>
+        /// 获取运行时的唯一id
+        /// </summary>
         long GetRuntimeId();
 
         void Terminate();
